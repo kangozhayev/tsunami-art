ae8930311874e2ce1df8468ebadc67e5653244c9
user.email <govorunkz@live.ru>
2025-08-09 21:41:12 +0500

    fix(Button): allow ReactNode for buttonText

--- src/common/button/Button.tsx
+++ src/common/button/Button.tsx
@@ -1,32 +1,16 @@
 import styles from './Button.module.scss';
 import clsx from 'clsx';
 
-type ButtonProps = {
-  buttonText: string;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  buttonText: React.ReactNode;
   className?: string;
-} & (
-  | (React.ButtonHTMLAttributes<HTMLButtonElement> & { href?: undefined })
-  | (React.AnchorHTMLAttributes<HTMLAnchorElement> & { href: string })
-);
+}
 
 export const Button = ({
   buttonText,
   className = '',
-  href,
   ...props
 }: ButtonProps) => {
-  if (href) {
-    return (
-      <a
-        href={href}
-        className={clsx(styles.button, className)}
-        {...(props as React.AnchorHTMLAttributes<HTMLAnchorElement>)}
-      >
-        {buttonText}
-      </a>
-    );
-  }
-
   return (
     <button
       className={clsx(styles.button, className)}
